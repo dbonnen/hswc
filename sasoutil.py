655f52c9622df746004d1e42b1dad0b09d959637
dbonnen <dbonnen@umich.edu>
2015-07-03 03:46:00 -0400

    whats happening

--- sasoutil.py
+++ sasoutil.py
@@ -558,6 +558,7 @@ def get_newest_sports_team(fandom, cursor):
 def check_pending_vote_entry(dwname,cursor):
     cursor.execute("SELECT COUNT(*) FROM pending_vote WHERE dwname=?",(dwname,))
     real_count = int(cursor.fetchone()[0])
+    print real_count
     if real_count > 0:
         return True
     else:

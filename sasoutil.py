11b4c3112a7ab39c4a1846c524513d1902991c88
dbonnen <dbonnen@umich.edu>
2015-07-05 02:01:30 -0400

    fix it up so that only the votes are applied to ONE person instead of all of them

--- sasoutil.py
+++ sasoutil.py
@@ -625,9 +625,9 @@ def enter_votes(dwname, vote1, vote2, vote3, cursor):
     cursor.execute("UPDATE mr1_team_votes SET votes = votes + 1 WHERE team_name = ?", (vote1,))
     cursor.execute("UPDATE mr1_team_votes SET votes = votes + 1 WHERE team_name = ?", (vote2,))
     cursor.execute("UPDATE mr1_team_votes SET votes = votes + 1 WHERE team_name = ?", (vote3,))
-    cursor.execute("UPDATE mr1_player_votes SET vote_1 = ?", (vote1,))
-    cursor.execute("UPDATE mr1_player_votes SET vote_2 = ?", (vote2,))
-    cursor.execute("UPDATE mr1_player_votes SET vote_3 = ?", (vote3,))
+    cursor.execute("UPDATE mr1_player_votes SET vote_1 = ? WHERE dwname = ?", (vote1, dwname,))
+    cursor.execute("UPDATE mr1_player_votes SET vote_2 = ? WHERE dwname = ?", (vote2, dwname,))
+    cursor.execute("UPDATE mr1_player_votes SET vote_3 = ? WHERE dwname = >", (vote3, dwname,))
     return 0
 
 if __name__ == "__main__":

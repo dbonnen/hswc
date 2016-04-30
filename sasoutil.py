d84efb3d055cdfeb7236dbff70f09422308c47b6
Indigo <hurristat@gmail.com>
2016-04-30 00:27:04 -0400

    whoaaa throwback. confused equality and assignment operators

--- sasoutil.py
+++ sasoutil.py
@@ -396,7 +396,7 @@ def get_team_display_line(team, cursor):
     stringofallplayers = ''
     for x in cursor.execute('SELECT * from players where team_id=?', (teamdatalist[0],)):
         stringofallplayers = stringofallplayers + ' ' + x[0]
-        if x[10] = 0:
+        if x[10] == 0:
             stringofallplayers = stringofallplayers + '*'
     stringofallplayers = stringofallplayers.strip()
     csstype = 'roster_teamslots'
